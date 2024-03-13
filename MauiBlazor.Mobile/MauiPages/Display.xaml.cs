b232fc226d859b3f6a7def8be90d0af4770fe3df
tugbadokumaci <cennetinagaci@gmail.com>
2024-03-13 10:49:25 +0300

    welcome pages added

--- MauiBlazor.Mobile/MauiPages/Display.xaml.cs
+++ MauiBlazor.Mobile/MauiPages/Display.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MauiPopup.Views;
+﻿using CommunityToolkit.Maui.Views;
+using MauiPopup;
+using MauiPopup.Views;
 
 namespace MauiBlazor.Mobile.MauiPages;
 
@@ -7,6 +9,11 @@ public partial class DisplayPage : BasePopupPage
 	public DisplayPage()
 	{
 		InitializeComponent();
-	}
+}
+
+    async void OkButton_Clicked(object sender, EventArgs e)
+    {
+        await PopupAction.ClosePopup();
+    }
 }
 
